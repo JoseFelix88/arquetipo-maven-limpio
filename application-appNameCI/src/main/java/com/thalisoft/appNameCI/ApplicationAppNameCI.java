913019488a566f61a2bb6727cfd235f26a85ab48
JOSE CURIEL <josecuriel88@gmail.com>
2021-09-08 21:08:38 -0500

    commit inicial de los modulos del arquetipo.
    Signed-off-by: JOSE CURIEL <josecuriel88@gmail.com>

---
+++ application-appNameCI/src/main/java/com/thalisoft/appNameCI/ApplicationAppNameCI.java
@@ -0,0 +1,13 @@
+package com.thalisoft.appNameCI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationAppNameCI {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationAppNameCI.class, args);
+	}
+
+}
